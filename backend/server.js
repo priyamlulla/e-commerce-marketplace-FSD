046f571ab9b4c99a0c552d9abc832402358f1763
priyamlulla <priyamlulla@gmail.com>
2026-09-23 16:43:01 +0530

    Complete Experiment 4 REST API and MongoDB CRUD

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const productRoutes = require("./routes/productRoutes");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use("/api/products", productRoutes);
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB Connected");
+  })
+  .catch((error) => {
+    console.error("MongoDB Connection Error:", error);
+  });
+
+app.get("/", (req, res) => {
+  res.send("Full Stack API is Running");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
